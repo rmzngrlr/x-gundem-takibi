22805372dc61acd10bf637d262356b5c7dc7fe0b
rmzngrlr <ramazan.rmzngrlr@gmail.com>
2025-12-15 10:45:48 +0300

    Delete baslat.bat

--- baslat.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Twitter Takip Sistemi Baslatiliyor... Lutfen bekleyin.
-echo ---------------------------------------------------
-streamlit run twitter_final.py
-pause
